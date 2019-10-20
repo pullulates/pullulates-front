5b3eb65194f679d1f4004c0d91573dc6b30e6bae
pullulates <t1heluosh1@163.com>
2019-10-20 23:00:32 +0800

    项目初始化

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -39,7 +39,7 @@ function hasRole(roles, route) {
 
 function filterAsyncRouter (routerMap, roles) {
   const accessedRouters = routerMap.filter(route => {
-    if (hasPermission(roles.permissionList, route)) {
+    if (hasPermission(roles, route)) {
       if (route.children && route.children.length) {
         route.children = filterAsyncRouter(route.children, roles)
       }
@@ -64,8 +64,7 @@ const permission = {
   actions: {
     GenerateRoutes ({ commit }, data) {
       return new Promise(resolve => {
-        const { roles } = data
-        const accessedRouters = filterAsyncRouter(asyncRouterMap, roles)
+        const accessedRouters = filterAsyncRouter(asyncRouterMap, data)
         commit('SET_ROUTERS', accessedRouters)
         resolve()
       })
